b7176114150e5152e486ae984cb165f74fb59544
AlexRSR <alexrobinsantiago@gmail.com>
2025-10-27 17:53:53 -0400

    Step 2
    File that contains policy information.

--- Project_Alex_SantiagoResendiz.java
+++ Project_Alex_SantiagoResendiz.java
@@ -5,79 +5,77 @@
 */
 
 import java.util.Scanner;
-
+import java.util.ArrayList;
+import java.io.*;
 public class Project_Alex_SantiagoResendiz
 {
-   public static void main(String[] args)
+   public static void main(String[] args) throws IOException
    {  
       // Create an instance of the Scanner class.
       Scanner keyboard = new Scanner(System.in);
       
-      // Create an instance of the Policy class.
-      Policy userPolicy = new Policy();
+      // Create an ArrayList to hold Policy objects.
+      ArrayList<Policy> list = new ArrayList<Policy>();
       
-      // Call the setInfo method, to input the information.
-      setInfo(keyboard, userPolicy);
+      // Call the setInfo method, to input the information from the txt file to ArrayList.
+      setInfo(list);
       
-      // Call the getInfo method, to display the information.
-      getInfo(userPolicy);
+      // The ArrayList will be passed to display every instance of the policy object's user info.
+      getInfo(list);
    }
    
    /**
-      setInfo will store all the infomation provide from the user into the
-      policy's class reference variable for future use.
-      @param input The reference variable to the Scanner class.
-      @param policy The reference variable to the Policy class.
+      setInfo will store all the infomation provide from the Text file into the
+      policy's ArrayList by adding it as a new instance in memory.
+      @param list The reference variable to the ArrayList that can store Policy objects.
    */
-   public static void setInfo(Scanner input, Policy policy)
+   public static void setInfo(ArrayList<Policy> list) throws IOException
    {
       // Temp Variables to hold the values
       int tempNum = 0, tempAge = 0;
       String provider = "", fName = "", lName = "", smokeStatus = "";
       double tempHeight = 0.0, tempWeight = 0.0;
       
-      // Since, the information was passed during the Policy class instantiation
-      // The program will call the policy class mutator methods to store the values from the variables.
-      System.out.print("Please enter the Policy Number: ");
-      tempNum = input.nextInt();
-      policy.setPolicyNum(tempNum);
-      
-      input.nextLine();
-      
-      System.out.print("Please enter the Provider Name: ");
-      provider = input.nextLine();
-      policy.setProvider(provider);
-      System.out.print("Please enter the Policyholder's First Name: ");
-      fName = input.nextLine();
-      policy.setFirstName(fName);
-      System.out.print("Please enter the Policyholder's Last Name: ");
-      lName = input.nextLine();
-      policy.setLastName(lName);
-      
-      
-      System.out.print("Please enter the Policyholder's Age: ");
-      tempAge = input.nextInt();
-      policy.setAge(tempAge);
-      
-      input.nextLine();
-      
-      System.out.print("Please enter the Policyholder's Smoking Status (smoker/non-smoker): ");
-      smokeStatus = input.nextLine();
-      policy.setSmoke(smokeStatus);
-      
-      System.out.print("Please enter the Policyholder's Height (in inches): ");
-      tempHeight = input.nextDouble();
-      policy.setHeight(tempHeight);
-      System.out.print("Please enter the Policyholder's Weight (in pounds): ");
-      tempWeight = input.nextDouble();
-      policy.setWeight(tempWeight);
+      // Create a try-with-resources statement to automatically close the file object.
+      try (Scanner inputFile = new Scanner(new File("PolicyInformation.txt")))
+      {
+         // This while statement will continue until there isn't any text in the Text file.
+         while (inputFile.hasNext()) {
+            tempNum = inputFile.nextInt();
+            // Clear the buffer
+            inputFile.nextLine();
+            provider = inputFile.nextLine();
+            fName = inputFile.nextLine();
+            lName = inputFile.nextLine();
+            tempAge = inputFile.nextInt();
+            // Create the buffer
+            inputFile.nextLine();
+            smokeStatus = inputFile.nextLine();
+            tempHeight = inputFile.nextDouble();
+            tempWeight = inputFile.nextDouble();
+            
+            // Knowing that theres already a two line seperation between each user's
+            // information, allow inputFile reference variable to skip over lines to the next user.
+            if (inputFile.hasNext()) {
+               inputFile.nextLine();
+            }
+            if (inputFile.hasNext()) {
+               inputFile.nextLine();
+            }
+            
+            // Finally for each individual user, they'll be added separately into the list ArrayList
+            // That will stored in memory as a Policy object. Additionally, every information gathered
+            // will be included into the instance.
+            list.add(new Policy(tempNum, provider, fName, lName, tempAge, smokeStatus, tempHeight, tempWeight));
+         }
+      }
    }
    
    /**
       The getInfo method will display the user's their own inputs,
       as well as the calculations to thier BMI and Policy price.
    */
-   public static void getInfo(Policy user)
+   public static void getInfo(ArrayList<Policy> list)
    {
       // Display the user's input.
       System.out.println("\n\nPolicy Number: " + user.getPolicyNum());
